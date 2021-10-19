a6de66b42fd3d7e5ea2ecb99bf4174084c704162
cplassais <plassais.christophe@gmail.com>
2021-10-19 11:34:04 +0200

    19-10-2021 11:33 requetes methodes génériques

--- controllers/subject/subjectAddController.php
+++ controllers/subject/subjectAddController.php
@@ -1,10 +1,9 @@
 <?php
-//require_once 'models/connexion.php';
 
-$oSubjects = new Subject();
+//$oSubjects = new Subject();
 if(!empty($_POST)):
-    $oSubjects->addSubject($dbc,$_POST['name'],$_POST['description'],$_POST['duration'],$_POST['coefficient']);
-//    $aParamsURL[2] = $_POST['id'];
+    Subject::addSubject($dbc, $_POST['name'],$_POST['description'],$_POST['duration'],$_POST['coefficient']);
+    //$aParamsURL[2] = $_POST['id'];
     header('Location: /subjectlist/');
 endif;
 $actionForm = '/subjectadd/';
