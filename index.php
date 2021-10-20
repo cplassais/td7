9652d3de4f2884fefe8bb2de8ea5362f708b6c9d
cplassais <plassais.christophe@gmail.com>
2021-10-20 11:13:20 +0200

    20-10-2021 11:14 ok

--- index.php
+++ index.php
@@ -6,11 +6,10 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>TD 7</title>
-        <link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
+        <link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"> <link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
-    <body class="container" style=" width: 85%;">
+    <body class="container">
         <header class="text-center bg-black fs-1 text-white"><h1>TD 7</h1>
             <h2 class="text-center mx-auto my-3">Test Bases de données PDO</h2>
         </header>
--- views/subjectListView.php
+++ views/subjectListView.php
@@ -1,5 +1,5 @@
 
-<table class="table table-striped text-center">
+<table class="table table-striped table-bordered table-hover table-sm text-center">
     <thead>
     <tr>
         <th scope="col">INDEX</th>
@@ -20,13 +20,23 @@
             <td><?php echo $response->coefMessage(); ?></td>
             <td><?php echo $response->getDurationByDay(); ?></td>
             <td><a type="button" class="btn btn-outline-success" href="/subjectsingle/<?php echo $response->getId(); ?>"
-                   title="modifier">Détails</a>
+                   title="détails"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+                        <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                        <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                    </svg></a>
                 <a type="button" class="btn btn-outline-dark" href="/subjectupdate/<?php echo $response->getId(); ?>"
-                   title="editer">Editer</a>
+                   title="editer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                    </svg></a>
                 <a type="button" class="btn btn-outline-info" href="/subjectduplicate/<?php echo $response->getId(); ?>"
-                   title="dupliquer">Dupliquer</a>
+                   title="dupliquer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-view-stacked" viewBox="0 0 16 16">
+                        <path d="M3 0h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3zm0 8h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3z"/>
+                    </svg></a>
                 <a type="button" class="btn btn-outline-danger" href="/subjectdelete/<?php echo $response->getId(); ?>"
-                title="supprimer">Supprimer</a>
+                title="supprimer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                    </svg></a>
             </td>
             <td></td>
         </tr>
